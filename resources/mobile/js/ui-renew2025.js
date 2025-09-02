89e493d703250253c9bc1038f35461dd751f1d85
chuljoolee <jangel@frameout.co.kr>
2025-09-02 09:13:16 +0900

    js 정리

--- resources/mobile/js/ui-renew2025.js
+++ resources/mobile/js/ui-renew2025.js
@@ -30,31 +30,6 @@ $(document).ready(function(){
     $('.all-menu').removeClass('--show');
     $('body').removeClass('no-scroll'); // 스크롤 해제
   });
-  /*
-
-  */
-
-  //v2
-  /*
-  let scrollY = 0;
-
-  $('.allmenu-js').on('click', function(){
-    scrollY = window.scrollY; // 현재 스크롤 위치 저장
-    $('body').addClass('no-scroll').css({
-      top: -scrollY + 'px'
-    });
-    $('.all-menu').addClass('--show');
-  });
-
-  $('#btnCloseJs').on('click', function(){
-    $('body').removeClass('no-scroll').css({
-      top: ''
-    });
-    $('.all-menu').removeClass('--show');
-    window.scrollTo(0, scrollY); // 원래 위치 복원
-  });
-  */
-
 });
 
 
@@ -71,19 +46,5 @@ $(document).ready(function(){
     $(this).addClass('on');
     $(this).next('.depth2-item-list').show();
   });
-
-  /** 2뎁스 메뉴 클릭 (dep 클래스가 있는 것만) */
-  /*
-   $('.nav_allmenu_list .depth2-item-list > li > a.dep').on('click', function(e){
-    e.preventDefault();
-
-    // 같은 depth2 안에서 토글
-    $(this).closest('.depth2-item-list').find('> li > a.dep').removeClass('on');
-    $(this).closest('.depth2-item-list').find('.depth3-item-list').hide();
-
-    $(this).addClass('on');
-    $(this).next('.depth3-item-list').show();
-  });
-  */
 
 });
